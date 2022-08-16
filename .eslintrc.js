c27014960da21387557e3e7a413cd9510c6e633c
Morilence <1280659615@qq.com>
2022-08-17 00:54:54 +0800

    chore: added eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        requireConfigFile: false, // for @babel/eslint-parser
+        ecmaVersion: 6,
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+
+    rules: {
+        "brace-style": ["error", "1tbs", { allowSingleLine: true }],
+        "comma-spacing": ["error", { before: false, after: true }],
+        "array-bracket-spacing": ["error", "never"],
+        "no-constant-condition": "off",
+        "no-undef": "warn",
+        "no-unused-vars": "warn",
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    },
+};
